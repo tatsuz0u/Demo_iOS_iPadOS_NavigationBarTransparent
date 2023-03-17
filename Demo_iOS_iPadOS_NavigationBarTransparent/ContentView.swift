0faf84c7968197410d459aeda0ec7c18ced287fc
Masatatsu So <m_so@yumemi.co.jp>
2023-03-17 15:46:25 +0900

    Update for iPadOS

--- Demo_iOS_iPadOS_NavigationBarTransparent/ContentView.swift
+++ Demo_iOS_iPadOS_NavigationBarTransparent/ContentView.swift
@@ -13,36 +13,66 @@ let count = 3
 struct ContentView: View {
     @State private var selectedIndex = 0
     @State private var lists = [[Int]](repeating: [], count: count)
+    @State private var columnVisibility = NavigationSplitViewVisibility.all
     @State private var hasGeneratedListOnce = [Bool](repeating: false, count: count)
 
-    var body: some View {
-        NavigationStack {
-            VStack {
-                Picker("", selection: $selectedIndex) {
-                    ForEach(0..<count, id: \.self) { index in
-                        Text(index.description).tag(index)
-                    }
-                }
-                .pickerStyle(.segmented)
-                .padding(.horizontal)
-
-                ForEach(0..<count, id: \.self) { index in
-                    GeneratorCell(
-                        tag: index,
-                        list: $lists[index],
-                        hasGeneratedOnce: $hasGeneratedListOnce[index]
-                    )
-                }
-
-                NavigationLink(
-                    destination: { DetailView(selectedIndex: selectedIndex, lists: lists) },
-                    label: { Text("Next") }
-                )
-                .buttonStyle(.borderedProminent)
-                .buttonBorderShape(.capsule)
-            }
-            .navigationTitle("Demo")
+    var generatorCells: some View {
+        ForEach(0..<count, id: \.self) { index in
+            GeneratorCell(
+                tag: index,
+                list: $lists[index],
+                hasGeneratedOnce: $hasGeneratedListOnce[index]
+            )
         }
+    }
+
+    var phoneLayout: some View {
+        VStack {
+            Picker("", selection: $selectedIndex) {
+                ForEach(0..<count, id: \.self) { index in
+                    Text(index.description).tag(index)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+
+            generatorCells
+
+            NavigationLink(
+                destination: { DetailView(selectedIndex: selectedIndex, lists: lists) },
+                label: { Text("Next") }
+            )
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+        }
+        .navigationTitle("Demo")
+    }
+
+    var body: some View {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            NavigationStack {
+                phoneLayout
+            }
+        } else if UIDevice.current.userInterfaceIdiom == .pad {
+            NavigationSplitView(
+                columnVisibility: $columnVisibility,
+                sidebar: { generatorCells.navigationTitle("Demo") },
+                content: {
+                    List(selection: $selectedIndex.asOptional) {
+                        ForEach(0..<count, id: \.self) { index in
+                            NavigationLink(index.description, value: index)
+                        }
+                    }
+                },
+                detail: { DetailView(selectedIndex: selectedIndex, lists: lists) }
+            )
+        }
+    }
+}
+
+extension Binding {
+    var asOptional: Binding<Value?> {
+        .init(get: { wrappedValue }, set: { if let value = $0 { wrappedValue = value } })
     }
 }
 
